228b33d0c29107d593f75526be27447037f0b3dd
Tsoorh <tsoor.hartov@gmail.com>
2025-10-11 19:58:13 +0300

    feat: added .active style and logic on filter

--- src/cmps/StayFilter.jsx
+++ src/cmps/StayFilter.jsx
@@ -1,138 +1,129 @@
-import { useState, useEffect } from 'react';
-import SearchIcon from '@mui/icons-material/Search';
+import { useState, useEffect } from "react";
+import SearchIcon from "@mui/icons-material/Search";
 
 export function StayFilter({ filterBy, setFilterBy }) {
-    // const [ filterToEdit, setFilterToEdit ] = useState(structuredClone(filterBy))
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
+  // const [ filterToEdit, setFilterToEdit ] = useState(structuredClone(filterBy))
 
-    // useEffect(() => {
-    //     setFilterBy(filterToEdit)
-    // }, [filterToEdit])
+  // useEffect(() => {
+  //     setFilterBy(filterToEdit)
+  // }, [filterToEdit])
 
-    // function handleChange(ev) {
-    //     const type = ev.target.type
-    //     const field = ev.target.name
-    //     let value
+  // function handleChange(ev) {
+  //     const type = ev.target.type
+  //     const field = ev.target.name
+  //     let value
 
-    //     switch (type) {
-    //         case 'text':
-    //         case 'radio':
-    //             value = field === 'sortDir' ? +ev.target.value : ev.target.value
-    //             if(!filterToEdit.sortDir) filterToEdit.sortDir = 1
-    //             break
-    //         case 'number':
-    //             value = +ev.target.value || ''
-    //             break
-    //     }
-    //     setFilterToEdit({ ...filterToEdit, [field]: value })
-    // }
+  //     switch (type) {
+  //         case 'text':
+  //         case 'radio':
+  //             value = field === 'sortDir' ? +ev.target.value : ev.target.value
+  //             if(!filterToEdit.sortDir) filterToEdit.sortDir = 1
+  //             break
+  //         case 'number':
+  //             value = +ev.target.value || ''
+  //             break
+  //     }
+  //     setFilterToEdit({ ...filterToEdit, [field]: value })
+  // }
 
-    // function clearFilter() {
-    //     setFilterToEdit({ ...filterToEdit, txt: '', minSpeed: '', maxPrice: '' })
-    // }
-    
-    // function clearSort() {
-    //     setFilterToEdit({ ...filterToEdit, sortField: '', sortDir: '' })
-    // }
+  // function clearFilter() {
+  //     setFilterToEdit({ ...filterToEdit, txt: '', minSpeed: '', maxPrice: '' })
+  // }
 
-    return <section className="Stay-filter">
-            {/* <h3>Filter:</h3> */}
-            {/* <input
-                type="text"
-                name="txt"
-                value={filterToEdit.txt}
-                placeholder="Free text"
-                onChange={handleChange}
-                required
-            />
-            <input
-                type="number"
-                min="0"
-                name="minSpeed"
-                value={filterToEdit.minSpeed}
-                placeholder="min. speed"
-                onChange={handleChange}
-                required
-            />
-            <button 
-                className="btn-clear" 
-                onClick={clearFilter}>Clear</button>
-            <h3>Sort:</h3>
-            <div className="sort-field">
-                <label>
-                    <span>Speed</span>
-                    <input
-                        type="radio"
-                        name="sortField"
-                        value="speed"
-                        checked={filterToEdit.sortField === 'speed'}
-                        onChange={handleChange}
-                    />
-                </label>
-                <label>
-                    <span>Vendor</span>
-                    <input
-                        type="radio"
-                        name="sortField"
-                        value="vendor"
-                        checked={filterToEdit.sortField === 'vendor'}            
-                        onChange={handleChange}
-                    />
-                </label>
-            </div>
-            <div className="sort-dir">
-                <label>
-                    <span>Asce</span>
-                    <input
-                        type="radio"
-                        name="sortDir"
-                        value="1"
-                        checked={filterToEdit.sortDir === 1}                        
-                        onChange={handleChange}
-                    />
-                </label>
-                <label>
-                    <span>Desc</span>
-                    <input
-                        type="radio"
-                        name="sortDir"
-                        value="-1"
-                        onChange={handleChange}
-                        checked={filterToEdit.sortDir === -1}                        
-                    />
-                </label>
-            </div>
-            <button 
-                className="btn-clear" 
-                onClick={clearSort}>Clear</button> */}
-                <button className='filter-btn flex column'>
-                <label htmlFor="">Where</label>
-                <input 
-                type="text" 
-                placeholder='Search destinations'
-                />
-                </button>
-                <button className='filter-btn flex column'>
-                <label htmlFor="">Check in</label>
-                <input 
-                type="date" 
-                placeholder='Add dates'
-                />
-                </button>
-                <button className='filter-btn flex column'>
-                <label htmlFor="">Check out</label>
-                <input 
-                type="date" 
-                placeholder='Add dates'
-                />
-                </button>
-                <button className='filter-btn flex column'>
-                <label htmlFor="">Who</label>
-                <input type="text" 
-                placeholder='Add guests'
-                />
-                </button>
-                <button className='search-btn'>
-                    <SearchIcon/>
-                </button>
-    </section>
+  // function clearSort() {
+  //     setFilterToEdit({ ...filterToEdit, sortField: '', sortDir: '' })
+  // }
+
+  function onHandleOpenFilter(ev) {
+    setIsFilterOpen(true);
+    onHandleClick(ev);
+  }
+
+  function onHandleClick({ target }) {
+    const filterContainer = document.querySelector(".stay-filter");
+    filterContainer.classList.add("active");
+    const allButtons = document.querySelectorAll(".filter-btn");
+    allButtons.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    const { name } = target;
+    target.classList.add("active");
+  }
+
+  if (isFilterOpen) {
+    return (
+        <section className="stay-filter">
+        <button
+          className="filter-btn flex column"
+          name="destination"
+          onClick={onHandleClick}
+        >
+          <label htmlFor="">Where</label>
+          <input type="text" placeholder="Search destinations" />
+        </button>
+        <button
+          className="filter-btn flex column"
+          name="check-in"
+          onClick={onHandleClick}
+        >
+          <label htmlFor="">Check in</label>
+          <input type="date" placeholder="Add dates" />
+        </button>
+        <button
+          className="filter-btn flex column"
+          name="check-out"
+          onClick={onHandleClick}
+        >
+          <label htmlFor="">Check out</label>
+          <input type="date" placeholder="Add dates" />
+        </button>
+        <button
+          className="filter-btn flex column"
+          name="guest"
+          onClick={onHandleClick}
+        >
+          <label htmlFor="">Who</label>
+          <input type="text" placeholder="Add guests" />
+        </button>
+        <button className="search-btn">
+          <SearchIcon />
+        </button>
+      </section>
+    );
+  } else {
+    return (
+      <section className="stay-filter">
+        <button
+          className="filter-btn flex column"
+          onClick={onHandleOpenFilter}
+          name="destination"
+          id="destination"
+        >
+          Anywhere
+        </button>
+        <button
+          className="filter-btn flex column"
+          onClick={onHandleOpenFilter}
+          name="time"
+          id="time"
+        >
+          AnyTime
+        </button>
+        <button
+          className="filter-btn flex column"
+          onClick={onHandleOpenFilter}
+          name="guest"
+          id="guest"
+        >
+          Add guests
+        </button>
+        <button className="search-btn small-search"
+        onClick={()=>{setIsFilterOpen(true)}}
+        >
+          <SearchIcon />
+        </button>
+      </section>
+    );
+  }
 }
