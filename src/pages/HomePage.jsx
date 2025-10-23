6c8bea51a789b0e73cc5fc04773927d85abb852d
naor554 <naoryoyo@gmail.com>
2025-10-23 15:55:46 +0300

    adding more fake data and modify stayIndex

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,21 +1,6 @@
-import { useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { loadStays } from '../store/actions/stay.actions'
-import { StayList } from '../cmps/StayList'
+import { StayIndex } from './StayIndex'
 
 export function HomePage() {
-    const stays = useSelector(storeState => storeState.stayModule.stays)
-
-    useEffect(() => {
-        loadStays()
-    }, [])
-
-    return (
-        <section className="home">
-            <div className="home-content">
-                <StayList stays={stays} />
-            </div>
-        </section>
-    )
+    return <StayIndex />
 }
 
